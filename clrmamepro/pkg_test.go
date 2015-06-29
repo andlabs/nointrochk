4826fff77014c4dce2a2f0ff3b0eb5ecf66a09d0
Pietro Gagliardi <pietro10@mac.com>
2015-06-29 15:51:53 -0400

    More clrmamepro package testing.

--- clrmamepro/pkg_test.go
+++ clrmamepro/pkg_test.go
@@ -1,4 +1,4 @@
-// <codedate
+// 29 june 2015
 package clrmamepro
 
 import (
@@ -30,25 +30,76 @@ game (
 	description "[BIOS] LaserActive (Japan) (v1.02)"
 	rom ( name "[BIOS] LaserActive (Japan) (v1.02).md" size 131072 crc 00EEDB3A md5 A5A2F9AAE57D464BC66B80EE79C3DA6E sha1 26237B333DB4A4C6770297FA5E655EA95840D5D9 )
 )
-
-game (
-	name "[BIOS] LaserActive (USA) (v1.04)"
-	description "[BIOS] LaserActive (USA) (v1.04)"
-	rom ( name "[BIOS] LaserActive (USA) (v1.04).md" size 131072 crc 50CD3D23 md5 0E7393CD0951D6DDE818FCD4CD819466 sha1 AA811861F8874775075BD3F53008C8AAF59B07DB )
-)
-
-game (
-	name "[BIOS] Mega-CD (Japan) (1.00S)"
-	description "[BIOS] Mega-CD (Japan) (1.00S)"
-	rom ( name "[BIOS] Mega-CD (Japan) (1.00S).md" size 131072 crc 79F85384 md5 A3DDCC8483B0368141ADFD99D9A1E466 sha1 230EBFC49DC9E15422089474BCC9FA040F2C57EB )
-)
-
-game (
-	name "[BIOS] Mega-CD (Japan) (1.00l)"
-	description "[BIOS] Mega-CD (Japan) (1.00l)"
-	rom ( name "[BIOS] Mega-CD (Japan) (1.00l).md" size 131072 crc F18DDE5B md5 29AD9CE848B49D0F9CEFC294137F653C sha1 0D5485E67C3F033C41D677CC9936AFD6AD618D5F )
-)
 `
+
+var expected = []*Block{
+	&Block{
+		Name:	"clrmamepro",
+		Texts:	map[string]string{
+			"name":		"Sega - Mega Drive - Genesis",
+			"description":	"Sega - Mega Drive - Genesis",
+			"version":		"20150628-135736",
+			"comment":	"no-intro | www.no-intro.org",
+		},
+	},
+	&Block{
+		Name:	"game",
+		Texts:	map[string]string{
+			"name":		"[BIOS] CDX (USA) (v2.21X)",
+			"description":	"[BIOS] CDX (USA) (v2.21X)",
+		},
+		Blocks:	map[string]*Block{
+			"rom":	&Block{
+				Name:	"rom",
+				Texts:	map[string]string{
+					"name":	"[BIOS] CDX (USA) (v2.21X).md",
+					"size":	"131072",
+					"crc":	"D48C44B5",
+					"md5":	"BACA1DF271D7C11FE50087C0358F4EB5",
+					"sha1":	"2B125C0545AFA089B617F2558E686EA723BDC06E",
+				},
+			},
+		},
+	},
+	&Block{
+		Name:	"game",
+		Texts:	map[string]string{
+			"name":		"[BIOS] LaserActive (USA) (v1.02)",
+			"description":	"[BIOS] LaserActive (USA) (v1.02)",
+		},
+		Blocks:	map[string]*Block{
+			"rom":	&Block{
+				Name:	"rom",
+				Texts:	map[string]string{
+					"name":	"[BIOS] LaserActive (USA) (v1.02).md",
+					"size":	"131072",
+					"crc":	"3B10CF41",
+					"md5":	"691C3FD368211280D268645C0EFD2EFF",
+					"sha1":	"8AF162223BB12FC19B414F126022910372790103",
+				},
+			},
+		},
+	},
+	&Block{
+		Name:	"game",
+		Texts:	map[string]string{
+			"name":		"[BIOS] LaserActive (Japan) (v1.02)",
+			"description":	"[BIOS] LaserActive (Japan) (v1.02)",
+		},
+		Blocks:	map[string]*Block{
+			"rom":	&Block{
+				Name:	"rom",
+				Texts:	map[string]string{
+					"name":	"[BIOS] LaserActive (Japan) (v1.02).md",
+					"size":	"131072",
+					"crc":	"00EEDB3A",
+					"md5":	"A5A2F9AAE57D464BC66B80EE79C3DA6E",
+					"sha1":	"26237B333DB4A4C6770297FA5E655EA95840D5D9",
+				},
+			},
+		},
+	},
+}
 
 func TestClrmamepro(t *testing.T) {
 	r := strings.NewReader(example)
@@ -56,11 +107,23 @@ func TestClrmamepro(t *testing.T) {
 	if err != nil {
 		t.Fatalf("error creating new datfile: %v", err)
 	}
-	for {
+	for i, e := range expected {
+		// TODO change to Next()
 		b, err := d.GetBlock()
-		if b == nil && err == nil {
-			break
+		if err != nil {
+			t.Fatalf("error reading block: %v", err)
 		}
-		t.Log(b, err)
+		if b == nil {
+			t.Fatalf("unexpected end of stream reading block %d", i)
+		}
+		// TODO
+		t.Log(b)
+	}
+	b, err := d.GetBlock()
+	if err != nil {
+		t.Fatalf("error finishing reading blocks: %v", err)
+	}
+	if b != nil {
+		t.Fatalf("another block was returned when no more were expected")
 	}
 }
