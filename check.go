b05dbbdc040a0e05fb7f80763e9aa3b9699b49fa
Pietro Gagliardi <pietro10@mac.com>
2015-06-29 17:55:25 -0400

    Added the actual checksumming.

--- check.go
+++ check.go
@@ -6,6 +6,11 @@ import (
 	"os"
 	"io"
 	"path/filepath"
+	"hash"
+	"hash/crc32"
+	"crypto/md5"
+	"crypto/sha1"
+	"strings"
 	"github.com/andlabs/nointrochk/clrmamepro"
 )
 
@@ -23,7 +28,32 @@ const (
 var nroms, ngood, nbad, nmiss uint
 
 func checksumsPass(romname string, rom *clrmamepro.Block, f io.Reader) bool {
-	// TODO
+	crc := crc32.NewIEEE()
+	md := md5.New()
+	sha := sha1.New()
+	checksums := io.MultiWriter(crc, md, sha)
+
+	_, err := io.Copy(checksums, f)
+	if err != nil {
+		die("checksumming ROM %q: %v", romname, err)
+	}
+
+	compare := func(h hash.Hash, expected string) bool {
+		return strings.ToUpper(fmt.Sprintf("%x", h.Sum(nil))) == strings.ToUpper(expected)
+	}
+	if !compare(crc, rom.Texts[fCRC32]) {
+		alert("BAD CRC32", romname)
+		return false
+	}
+	if !compare(md, rom.Texts[fMD5]) {
+		alert("BAD MD5", romname)
+		return false
+	}
+	if !compare(sha, rom.Texts[fSHA1]) {
+		alert("BAD SHA1", romname)
+		return false
+	}
+
 	return true
 }
 
